5fa442bf4f959b4926747b767bc0f41cbc922052
Branan Purvine-Riley <branan@puppetlabs.com>
2012-09-13 08:04:01 -0700

    Remove 'creates' from exec resource since this is based on unless in the released version

--- manifests/init.pp
+++ manifests/init.pp
@@ -7,6 +7,5 @@ define s3file (
     path    => ['/bin', '/usr/bin', 'sbin', '/usr/sbin'],
     command => "curl -L -o ${name} ${real_source}",
     unless  => "[ -e ${name} ] && curl -I ${real_source} | grep ETag | grep `md5sum ${name} | cut -c1-32`",
-    creates => $name,
   }
 }
