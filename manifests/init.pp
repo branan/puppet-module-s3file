94b9e9b248fdd89c7c293d74a51f82fd087da4a2
Branan Purvine-Riley <branan@puppetlabs.com>
2012-09-13 11:57:15 -0700

    Make the S3file define ensurable

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,11 +1,29 @@
 define s3file (
   $source,
+  $ensure = 'latest',
 )
 {
-  $real_source = "https://s3.amazonaws.com/${source}"
-  exec { "fetch ${name}":
-    path    => ['/bin', '/usr/bin', 'sbin', '/usr/sbin'],
-    command => "curl -L -o ${name} ${real_source}",
-    unless  => "[ -e ${name} ] && curl -I ${real_source} | grep ETag | grep `md5sum ${name} | cut -c1-32`",
+  $valid_ensures = [ 'absent', 'present', 'latest' ]
+  validate_re($ensure, $valid_ensures)
+
+  if $ensure == 'absent' {
+    # We use a puppet resource here to force the file to absent state
+    file { $name:
+      ensure => absent
+    }
+  } else {
+    $real_source = "https://s3.amazonaws.com/${source}"
+
+    if $ensure == 'latest' {
+      $unless = "[ -e ${name} ] && curl -I ${real_source} | grep ETag | grep `md5sum ${name} | cut -c1-32`"
+    } else {
+      $unless = "[ -e ${name} ]"
+    }
+
+    exec { "fetch ${name}":
+      path    => ['/bin', '/usr/bin', 'sbin', '/usr/sbin'],
+      command => "curl -L -o ${name} ${real_source}",
+      unless  => $unless
+    }
   }
 }
