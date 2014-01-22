80ff6570b73aa9b3eebc10b366341018a6b56083
Alex Linden Levy <lindenle@gmail.com>
2014-01-21 17:52:06 -0700

    allow s3file to use ec2 keys

--- manifests/init.pp
+++ manifests/init.pp
@@ -6,8 +6,10 @@
 # - $name: The local target of the file
 # - $source: The bucket and filename on S3
 # - $ensure: 'present', 'absent', or 'latest': as the core File resource
-# - $s3_domain: s3 server to fetch the file from
-#
+# - $s3_domain: s3 server url to fetch the file from including protocol (D:https://s3.amazonaws.com)
+# - $s3_access_key: access key for s3 api service
+# - $s3_private_key: private key for s3 signature
+# - $logoutput: log the curl output to the screen on puppet run
 # Requires:
 # - cURL
 #
@@ -21,7 +23,10 @@
 define s3file (
   $source,
   $ensure = 'latest',
-  $s3_domain = 's3.amazonaws.com',
+  $s3_domain = 'https://s3.amazonaws.com',
+  $s3_access_key = undef,
+  $s3_secret_key = undef,               
+  $logoutput = false,
 )
 {
   $valid_ensures = [ 'absent', 'present', 'latest' ]
@@ -33,7 +38,7 @@ define s3file (
       ensure => absent
     }
   } else {
-    $real_source = "https://${s3_domain}/${source}"
+    $real_source = "${s3_domain}/${source}"
 
     if $ensure == 'latest' {
       $unless = "[ -e ${name} ] && curl -I ${real_source} | grep ETag | grep `md5sum ${name} | cut -c1-32`"
@@ -41,9 +46,24 @@ define s3file (
       $unless = "[ -e ${name} ]"
     }
 
+    if $s3_secret_key == undef or $s3_access_key == undef
+    {
+      $curl_command = "curl -L -o ${name} ${real_source}"
+    }
+    else
+    {
+      # this makes the
+      $date = inline_template("<%= Time.now.to_s %>")
+      $payload = inline_template("GET\n\n\n<%=@date%>\n/<%= @source -%>")
+      $signature = inline_template("<% require 'base64' -%><% require 'openssl' -%><%= Base64.encode64( OpenSSL::HMAC.digest(OpenSSL::Digest::Digest.new('sha1'), @s3_secret_key, @payload.encode(Encoding::UTF_8) ) ).strip.to_s-%>")
+      $header = inline_template("Authorization: AWS <%=@s3_access_key-%>:<%=@signature-%>")
+      $curl_command = "curl -vL -o ${name} -X GET -H \"Date: ${date}\" -H \"${header}\" \"${real_source}\""
+    }
+
     exec { "fetch ${name}":
       path    => ['/bin', '/usr/bin', 'sbin', '/usr/sbin'],
-      command => "curl -L -o ${name} ${real_source}",
+      command => $curl_command,
+      logoutput => $logoutput,
       unless  => $unless
     }
   }
