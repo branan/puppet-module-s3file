b98424f05249420dcb89e83468b6ff0f6b3ac00f
uia3es <stevenelvar@gmail.com>
2018-01-09 15:25:41 +0000

    Cater for nested files

--- manifests/init.pp
+++ manifests/init.pp
@@ -28,7 +28,6 @@ define s3file (
 {
   $valid_ensures = [ 'absent', 'present', 'latest' ]
   validate_re($ensure, $valid_ensures)
-
   if $ensure == 'absent' {
     # We use a puppet resource here to force the file to absent state
     file { $name:
@@ -36,20 +35,19 @@ define s3file (
     }
   } else {
     if $vpc_endpoint == true {
-      $bucket_file = split($source, '/')
-      $bucket = $bucket_file[0]
-      $file = $bucket_file[1]
-      $real_source = "https://${bucket}.${s3_domain}/${file}"
+      $bucket_file_array = split($source, '/')
+      $bucket_array = [$bucket_file_array[0],]
+      $file_array = difference($bucket_file_array,$bucket_array)
+      $file = join($file_array, "/")
+      $real_source = "https://${bucket_array[0]}.${s3_domain}/${file}"
     } else {
       $real_source = "https://${s3_domain}/${source}"
     }
-
     if $ensure == 'latest' {
       $unless = "[ -e ${name} ] && curl -I ${real_source} | grep ETag | grep `md5sum ${name} | cut -c1-32`"
     } else {
       $unless = "[ -e ${name} ]"
     }
-
     exec { "fetch ${name}":
       path    => ['/bin', '/usr/bin', 'sbin', '/usr/sbin'],
       command => "curl -L -o ${name} ${real_source}",
